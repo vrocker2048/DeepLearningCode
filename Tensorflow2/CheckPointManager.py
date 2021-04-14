f49b53d779105b714f2ff6bb9b8209023aefb5fc
vrocker2048 <vrocker2048@gmail.com>
2021-04-14 20:24:46 +0800

    feat: keep lastest CheckPoint.

---
+++ Tensorflow2/CheckPointManager.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+import numpy as np
+import argparse
+from MLP import MLP, MNISTLoader
+
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--mode', default='train', help='train or test')
+parser.add_argument('--num_epochs', default=1)
+parser.add_argument('--batch_size', default=256)
+parser.add_argument('--learning_rate', default=0.001)
+args = parser.parse_args()
+data_loader = MNISTLoader()
+
+
+def train():
+    model = MLP()
+    optimizer = tf.keras.optimizers.Adam(learning_rate=args.learning_rate)
+    num_batches = int(data_loader.num_train_data // args.batch_size * args.num_epochs)
+    checkpoint = tf.train.Checkpoint(myAwesomeModel=model)
+    manager = tf.train.CheckpointManager(checkpoint, directory='./check_point', max_to_keep=3)
+    for batch_index in range(1, num_batches):
+        X, y = data_loader.get_batch(args.batch_size)
+        with tf.GradientTape() as tape:
+            y_pred = model(X)
+            loss = tf.keras.losses.sparse_categorical_crossentropy(y_true=y, y_pred=y_pred)
+            loss = tf.reduce_mean(loss)
+            print("batch %d: loss %f" % (batch_index, loss.numpy()))
+        grads = tape.gradient(loss, model.variables)
+        optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
+        if batch_index % 100 == 0:
+            path = manager.save(checkpoint_number=batch_index)
+            print("model saved to %s" % path)
+
+def test():
+    model_to_be_restored = MLP()
+    checkpoint = tf.train.Checkpoint(myAwesomeModel=model_to_be_restored)
+    checkpoint.restore(tf.train.latest_checkpoint('./check_point'))
+    y_pred = np.argmax(model_to_be_restored.predict(data_loader.test_data), axis=-1)
+    print("test accuracy: %f" % (sum(y_pred == data_loader.test_label) / data_loader.num_test_data))
+
+
+if __name__ == '__main__':
+    if args.mode == 'train':
+        train()
+    if args.mode == 'test':
+        test()
